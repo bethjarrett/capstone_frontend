167563bd2b677c17bc5cc2023348e7adab52925a
Beth <67609395+bethjarrett@users.noreply.github.com>
2022-10-23 21:47:06 -0400

    firebase & build

---
+++ build/precache-manifest.fcbc30579f78502360cf77eb3170fb6f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a28d93e157d7e0d072742c6c97c2fe9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cd2d09524d8ede0efe7b",
+    "url": "/static/css/main.f4fa1fcd.chunk.css"
+  },
+  {
+    "revision": "e898d94a776b75918597",
+    "url": "/static/js/2.6467d3a9.chunk.js"
+  },
+  {
+    "revision": "6680fcbc6da49a99053f2a189b46f41a",
+    "url": "/static/js/2.6467d3a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd2d09524d8ede0efe7b",
+    "url": "/static/js/main.3b3a98ab.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.3b3a98ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30fa6925e4e9de907612",
+    "url": "/static/js/runtime-main.1841f5a4.js"
+  },
+  {
+    "revision": "4629a586c4c7e26a9d5c023e055b7b40",
+    "url": "/static/media/fakeprofile.4629a586.jpg"
+  },
+  {
+    "revision": "5632ebf09e88e72e946e35284bef44ef",
+    "url": "/static/media/fakesquarefile.5632ebf0.jpg"
+  }
+]);
