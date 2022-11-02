94434b8b95217de6a5a029d96b3a61bbf9684f0f
Beth <67609395+bethjarrett@users.noreply.github.com>
2022-11-01 23:33:55 -0400

    updated navbar, footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import { FaLock } from 'react-icons/fa';
 
 const Footer = () => {
   return (
     <div className='text-center pb-4 container'>
       <hr />
-      &#169; <a href="https://bethjarrett.ca/">Beth Jarrett</a>, 2022
+      <p>&#169; <a href="https://bethjarrett.ca/">Beth Jarrett</a>, 2022</p>
+      <p><a href="/login"><FaLock /></a></p>
     </div>
   )
 }
--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,24 +1,18 @@
 import React from 'react';
 
-import {Link} from "react-router-dom";
-import { FaUserFriends, FaComments, FaGlobeAmericas, FaLock } from 'react-icons/fa';
+import { FaCommentDots, FaAddressCard, FaImages, FaRegCalendarAlt } from 'react-icons/fa';
 
 class Navigation extends React.Component {
     render() {
       return(
-<nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-    <div className="container"><Link to="/" className="navbar-brand">scrapebook</Link>
-        <div className="collapse navbar-collapse" id="fakebookNavbar">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                <li className="nav-item"><a className="nav-link"><FaUserFriends /></a></li>
-                <li className="nav-item"><a className="nav-link"><FaComments /></a></li>
-                <li className="nav-item"><a className="nav-link"><FaGlobeAmericas /></a></li>
-                <form><input className="form-control" type="text" placeholder="Search" aria-label="Search" /></form>
-            </ul>
-            <ul className="navbar-nav mb-2 mb-lg-0">
-                <li className="nav-item"><Link to="/login" className="nav-link"><FaLock /></Link></li>
-            </ul>
-        </div>
+<nav className="navbar navbar-expand navbar-dark bg-primary">
+    <div className="container-fluid"><a href="/" className="navbar-brand">scrapebook</a>
+        <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+            <li className='nav-ite'><a href="/" className="nav-link"><button type="button" className="btn btn-primary"><FaCommentDots /></button></a></li>
+            <li className="nav-item"><a className="nav-link"><button type="button" className="btn btn-primary" data-toggle="modal" data-target="#modalCenter"><FaAddressCard /></button></a></li>
+            <li className="nav-item"><a className="nav-link" href="/photos"><button type="button" className="btn btn-primary"><FaImages /></button></a></li>
+            <li className="nav-item"><a className="nav-link" href="/quiz"><button type="button" className="btn btn-primary"><FaRegCalendarAlt /></button></a></li>
+        </ul>
     </div>
 </nav>
         );
