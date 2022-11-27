a0eaba4199ba80cd7cb03ee6ed681eef6876b1cb
Beth <67609395+bethjarrett@users.noreply.github.com>
2022-11-27 15:18:44 -0500

    changed backend hosting

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const url = 'https://backendbeth.herokuapp.com/posts';
+const url = 'https://backend-zyts.onrender.com/posts';
 
 export const fetchPosts = () => axios.get(url);
 export const createPost = (newPost) => axios.post(url, newPost);
